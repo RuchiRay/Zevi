ded5a2cabf7b18bebbf1c23d44ed64caac69015d
RuchiRay <ruchiray18@gmail.com>
2023-12-28 16:55:08 +0530

    added logo to the gome page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-
+import logo from "../images/zeviLogo.png";
 export const Home = () => {
-  return <div className="home-container"></div>;
+  return (
+    <div className="home-container">
+      <div className="w-full logo-wrapper">
+        <div>
+          <img src={logo} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
