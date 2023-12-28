f2a2a92ebc4d67002a2e8cb60615179b597f7bec
RuchiRay <ruchiray18@gmail.com>
2023-12-29 01:00:25 +0530

    added fake results data

---
+++ src/data/results.ts
@@ -0,0 +1,157 @@
+import img1 from '../images/dress1.png'
+import img2 from '../images/dress2.png'
+import img3 from '../images/dress3.png'
+import img4 from '../images/dress4.png'
+import img5 from '../images/dress5.png'
+import img6 from '../images/dress6.png'
+import img7 from '../images/dress7.png'
+import img8 from '../images/dress8.jpg'
+import img9 from '../images/dress9.jpg'
+import img10 from '../images/dress10.jpg'
+import img11 from '../images/dress11.jpg'
+import img12 from '../images/dress12.jpg'
+import img13 from '../images/dress13.jpg'
+import img14 from '../images/dress14.jpg'
+
+export const results = [
+{
+    id:1,
+    img:img1,
+    title:"Long jacket",
+    oldPrice:1599,
+    newPrice:1349,
+    rating:5,
+    totalRating:210,
+    wishlisted:false
+},
+{
+    id:1,
+    img:img2,
+    title:"Stripped sweater",
+    oldPrice:1999,
+    newPrice:1749,
+    rating:4,
+    totalRating:212,
+    wishlisted:false
+},
+{
+    id:1,
+    img:img3,
+    title:"High waise trousers",
+    oldPrice:700,
+    newPrice:449,
+    rating:3,
+    totalRating:102,
+    wishlisted:false
+},
+{
+    id:1,
+    img:img4,
+    title:"High neck pullover ",
+    oldPrice:650,
+    newPrice:399,
+    rating:4,
+    totalRating:14,
+    wishlisted:false
+},
+{
+    id:1,
+    img:img5,
+    title:"Satin jumpsuit",
+    oldPrice:1299,
+    newPrice:1199,
+    rating:1,
+    totalRating:12,
+    wishlisted:false
+},
+{
+    id:1,
+    img:img6,
+    title:"Offshoulder top ",
+    oldPrice:699,
+    newPrice:349,
+    rating:5,
+    totalRating:23,
+    wishlisted:false
+},
+{
+    id:1,
+    img:img7,
+    title:"Round neck cotton Tee",
+    oldPrice:2400,
+    newPrice:2100,
+    rating:3,
+    totalRating:45,
+    wishlisted:false
+},
+{
+    id:1,
+    img:img8,
+    title:"yellow cord-set",
+    oldPrice:999,
+    newPrice:899,
+    rating:4,
+    totalRating:78,
+    wishlisted:false
+},
+{
+    id:1,
+    img:img9,
+    title:"Graphic tee",
+    oldPrice:699,
+    newPrice:599,
+    rating:3,
+    totalRating:34,
+    wishlisted:false
+},
+{
+    id:1,
+    img:img10,
+    title:"Corset and yellow pants",
+    oldPrice:1699,
+    newPrice:1399,
+    rating:2,
+    totalRating:21,
+    wishlisted:false
+},
+{
+    id:1,
+    img:img11,
+    title:"Pink floral dress",
+    oldPrice:849,
+    newPrice:749,
+    rating:4,
+    totalRating:67,
+    wishlisted:false
+},
+{
+    id:1,
+    img:img12,
+    title:"Black floral dress",
+    oldPrice:1349,
+    newPrice:1149,
+    rating:3,
+    totalRating:13,
+    wishlisted:false
+},
+{
+    id:1,
+    img:img13,
+    title:"black twirl skirt",
+    oldPrice:799,
+    newPrice:499,
+    rating:5,
+    totalRating:87,
+    wishlisted:false
+},
+{
+    id:1,
+    img:img14,
+    title:"Cargo",
+    oldPrice:1759,
+    newPrice:1449,
+    rating:2,
+    totalRating:130,
+    wishlisted:false
+},
+]
