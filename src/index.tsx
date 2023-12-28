79a2f750f1f275c607fc84c15ddfc4a2b68d8198
RuchiRay <ruchiray18@gmail.com>
2023-12-29 01:24:31 +0530

    added routing and shadow

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Results } from "./pages/Results/Results";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/results",
+    element: <Results />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
