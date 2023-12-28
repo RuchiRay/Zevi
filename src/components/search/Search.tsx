2a20ffed972cfd1c921b47c90d8e94acbd3d0af2
RuchiRay <ruchiray18@gmail.com>
2023-12-29 01:10:23 +0530

    added search icon

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,13 +1,13 @@
 import React, { Dispatch, SetStateAction } from "react";
 import "./search.scss";
-
+import { RiSearchLine } from "react-icons/ri";
 type SearchProps = {
   setOpenTrending: Dispatch<SetStateAction<boolean>>;
 };
 
 export const Search = ({ setOpenTrending }: SearchProps) => {
   return (
-    <div className="search-container">
+    <div className="search-container margin-top">
       <input
         type="text"
         name=""
@@ -15,6 +15,9 @@ export const Search = ({ setOpenTrending }: SearchProps) => {
         onClick={() => setOpenTrending(true)}
         placeholder="Search"
       />
+      <div className="icon">
+        <RiSearchLine />
+      </div>
     </div>
   );
 };
