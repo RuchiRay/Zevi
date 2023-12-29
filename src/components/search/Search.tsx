7b878aa1849cdeec0a933741277c0f42ec942ce4
RuchiRay <ruchiray18@gmail.com>
2023-12-29 17:38:31 +0530

    added navigation to results page

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,23 +1,28 @@
 import React, { Dispatch, SetStateAction } from "react";
 import "./search.scss";
 import { RiSearchLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
+
 type SearchProps = {
   setOpenTrending: Dispatch<SetStateAction<boolean>>;
 };
 
 export const Search = ({ setOpenTrending }: SearchProps) => {
+  const navigate = useNavigate();
   return (
     <div className="search-container ">
-      <input
-        type="text"
-        name=""
-        id=""
-        onClick={() => setOpenTrending(true)}
-        placeholder="Search"
-      />
-      <div className="icon">
-        <RiSearchLine />
-      </div>
+      <form action="" onSubmit={() => navigate("/results")}>
+        <input
+          type="text"
+          name=""
+          id=""
+          onClick={() => setOpenTrending(true)}
+          placeholder="Search"
+        />
+        <div className="icon">
+          <RiSearchLine />
+        </div>
+      </form>
     </div>
   );
 };
