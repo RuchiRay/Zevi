0e9d632cda9324017564ff224cbddc2de42093df
Ruchi Ray <ruchiray18@gmail.com>
2025-04-04 00:45:07 +0530

    test

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,11 +5,13 @@ import { Trending } from "../../components/trendingBox/Trending";
 import { Logo } from "../../components/logo/Logo";
 export const Home = () => {
   const [openTrending, setOpenTrending] = useState(false);
+  console.log("test");
+
   return (
     <div className="home-container">
       <Logo />
       <div className="margin-top"></div>
-      {/* <Search setOpenTrending={setOpenTrending} /> */}
+      <Search setOpenTrending={setOpenTrending} />
       {openTrending && <Trending />}
     </div>
   );
